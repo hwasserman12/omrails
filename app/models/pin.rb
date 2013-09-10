f14f5a8b484310ddd2073f804137bd43c942d6e2
Hannah Wasserman <hwasserman12@gmail.com>
2013-09-10 09:46:53 +0530

    Add Pins Assciation

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,7 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description
+  	attr_accessible :description
 
-validates :description, presence: true
+	validates :description, presence: true
+	belongs_to :user
+	validates :user_id, presence: true
 end
